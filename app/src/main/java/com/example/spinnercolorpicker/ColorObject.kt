35ffdd7423a53f53edc5765d20f472b56748e4ec
Luiza Vieira <luuhbam@gmail.com>
2022-08-22 13:03:45 -0300

    Second commit

--- app/src/main/java/com/example/spinnercolorpicker/ColorObject.kt
+++ app/src/main/java/com/example/spinnercolorpicker/ColorObject.kt
@@ -1,6 +1,6 @@
 package com.example.spinnercolorpicker
 
-class ColorObject(var name: String, var hex: String, var contrastHex: String)
+class ColorObject(var name: String, var hex: String, contrastHex: String)
 {
     val hexHash : String = hex
     val contrastHexHash : String = contrastHex

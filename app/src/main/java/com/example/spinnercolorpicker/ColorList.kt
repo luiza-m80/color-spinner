e29d376fef5e856622808375a971ad8ef42a6cf0
Luiza Vieira <luuhbam@gmail.com>
2022-08-24 15:41:36 -0300

    Projeto corrigido e finalizado.

--- app/src/main/java/com/example/spinnercolorpicker/ColorList.kt
+++ app/src/main/java/com/example/spinnercolorpicker/ColorList.kt
@@ -2,8 +2,8 @@ package com.example.spinnercolorpicker
 
 class ColorList
 {
-    private val blackHex = "000000"
-    private val whiteHex = "FFFFFF"
+    private val blackHex = "#000000"
+    private val whiteHex = "#FFFFFF"
 
     val defaultColor: ColorObject = basicColors()[0]
 
@@ -21,19 +21,19 @@ class ColorList
     {
         return listOf(
             ColorObject("Black", blackHex, whiteHex),
-            ColorObject("Silver", "C0C0C0", blackHex),
-            ColorObject("Gray", "808080", whiteHex),
-            ColorObject("Maroon", "800000", whiteHex),
-            ColorObject("Red", "FF0000", whiteHex),
-            ColorObject("Fuchsia", "FF00FF", whiteHex),
-            ColorObject("Green", "008000", whiteHex),
-            ColorObject("Lime", "00FF00", blackHex),
-            ColorObject("Olive", "808000", whiteHex),
-            ColorObject("Yellow", "FFFF00", blackHex),
-            ColorObject("Navy", "000080", whiteHex),
-            ColorObject("Blue", "0000FF", whiteHex),
-            ColorObject("Teal", "008080", whiteHex),
-            ColorObject("Aqua", "00FFFF", blackHex)
+            ColorObject("Silver", "#C0C0C0", blackHex),
+            ColorObject("Gray", "#808080", whiteHex),
+            ColorObject("Maroon", "#800000", whiteHex),
+            ColorObject("Red", "#FF0000", whiteHex),
+            ColorObject("Fuchsia", "#FF00FF", whiteHex),
+            ColorObject("Green", "#008000", whiteHex),
+            ColorObject("Lime", "#00FF00", blackHex),
+            ColorObject("Olive", "#808000", whiteHex),
+            ColorObject("Yellow", "#FFFF00", blackHex),
+            ColorObject("Navy", "#000080", whiteHex),
+            ColorObject("Blue", "#0000FF", whiteHex),
+            ColorObject("Teal", "#008080", whiteHex),
+            ColorObject("Aqua", "#00FFFF", blackHex)
         )
     }
 }
